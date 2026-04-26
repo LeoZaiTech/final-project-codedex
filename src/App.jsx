925aa0ee4875e52c10dff44ccff52572a0ccbdaf
LeoZaiTech <98131015+LeoZaiTech@users.noreply.github.com>
2026-04-26 19:33:55 -0400

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
+import Header from './components/header';
+
+
+
 function App() {
-  return <div>Hello Quiz</div>;
+  return (
+  <UserProvider>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<p>Home placeholder</p>} />
+      </Routes>
+  </UserProvider>
+  );
 }
 
 export default App;
